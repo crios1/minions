268f4d04fa0d4318b52a5b16c61a049b4fa1cbf0
Chris Rios <chrisrios586@gmail.com>
2026-09-11 03:29:04 -0400

    Remove value setters from fixed resource fixtures

--- tests/assets/resources/fixed/default.py
+++ tests/assets/resources/fixed/default.py
@@ -4,10 +4,6 @@ from tests.assets.support.resource_spied import SpiedResource
 class AssetResource(SpiedResource):
     _value = 123
 
-    @classmethod
-    def set_value(cls, value: int) -> None:
-        cls._value = value
-
     async def get_value(self) -> int:
         return type(self)._value
 
